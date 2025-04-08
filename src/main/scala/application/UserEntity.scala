92069026c7370e94bb8ca350e923f6c62e6caccb
firas frikha <firasfrikha@firass-MacBook-Pro.local>
2025-04-08 19:02:28 +0200

    prepare squeleton for states

--- src/main/scala/application/UserEntity.scala
+++ src/main/scala/application/UserEntity.scala
@@ -1,5 +1,53 @@
 package application
 
+import akka.persistence.typed.scaladsl.ReplyEffect
+
 object UserEntity {
 
+  sealed trait State {
+    def id: String
+
+    def applyCommand(command: Command): ReplyEffect[Event, State]
+
+    def applyEvent(event: Event): State
+  }
+
+  sealed trait Command
+
+  sealed trait Event
+
+  case class EmptyState(override val id: String) extends State {
+
+    override def applyCommand(command: Command): ReplyEffect[Event, State] = ???
+
+    override def applyEvent(event: Event): State = ???
+  }
+
+  case class PendingVerificationState(override val id: String) extends State {
+
+    override def applyCommand(command: Command): ReplyEffect[Event, State] = ???
+
+    override def applyEvent(event: Event): State = ???
+  }
+
+  case class ActiveState(override val id: String) extends State {
+
+    override def applyCommand(command: Command): ReplyEffect[Event, State] = ???
+
+    override def applyEvent(event: Event): State = ???
+  }
+
+  case class LockedState(override val id: String) extends State {
+
+    override def applyCommand(command: Command): ReplyEffect[Event, State] = ???
+
+    override def applyEvent(event: Event): State = ???
+  }
+
+  case class DeletedState(override val id: String) extends State {
+
+    override def applyCommand(command: Command): ReplyEffect[Event, State] = ???
+
+    override def applyEvent(event: Event): State = ???
+  }
 }
