1c689b30d6a8cee91501190c26683127b129c0e3
firas-frikha <frikha.firas@outlook.com>
2025-04-28 23:52:36 +0200

    implement command and event handler or the register command

--- src/main/scala/application/UserEntity.scala
+++ src/main/scala/application/UserEntity.scala
@@ -1,9 +1,8 @@
 package application
 
-import akka.actor.typed.Behavior
+import akka.actor.typed.{ActorRef, Behavior}
 import akka.persistence.typed.PersistenceId
-import akka.persistence.typed.scaladsl.EventSourcedBehavior
-import akka.persistence.typed.scaladsl.ReplyEffect
+import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior, ReplyEffect}
 
 object UserEntity {
 
@@ -20,7 +19,8 @@ object UserEntity {
   case class RegisterUserCommand(firstName: String,
                                  lastName: String,
                                  userId: String,
-                                 password: String) extends Command
+                                 password: String)
+                                (val replyTo: ActorRef[RegisterCommandResult]) extends Command
 
   sealed trait Event {
     def id: String
@@ -35,20 +35,58 @@ object UserEntity {
 
   sealed trait Result
 
-  sealed trait RegisterResult extends Result
+  sealed trait RegisterCommandResult extends Result
+
+  case object SuccessfulRegisterCommandUserCommand extends RegisterCommandResult
+
+  case class UnsupportedRegisterCommandUserCommand(reason: String) extends RegisterCommandResult
+
 
   case class EmptyState(override val id: String) extends State {
 
-    override def applyCommand(command: Command): ReplyEffect[Event, State] = ???
+    override def applyCommand(command: Command): ReplyEffect[Event, State] =
+      command match {
+        case registerCommand: RegisterUserCommand =>
+          Effect
+            .persist(RegisteredUserEvent(
+              id = registerCommand.userId,
+              firstName = registerCommand.firstName,
+              lastName = registerCommand.lastName,
+              userId = registerCommand.userId,
+              password = registerCommand.password
+            ))
+            .thenReply(registerCommand.replyTo)(_ => SuccessfulRegisterCommandUserCommand)
+      }
 
-    override def applyEvent(event: Event): State = ???
+    override def applyEvent(event: Event): State =
+      event match {
+        case registeredUserEvent: RegisteredUserEvent =>
+          PendingVerificationState(
+            id = registeredUserEvent.id,
+            firstName = registeredUserEvent.firstName,
+            lastName = registeredUserEvent.lastName,
+            userId = registeredUserEvent.userId,
+            password = registeredUserEvent.password)
+      }
   }
 
-  case class PendingVerificationState(override val id: String) extends State {
+  case class PendingVerificationState(override val id: String,
+                                      firstName: String,
+                                      lastName: String,
+                                      userId: String,
+                                      password: String) extends State {
 
-    override def applyCommand(command: Command): ReplyEffect[Event, State] = ???
+    override def applyCommand(command: Command): ReplyEffect[Event, State] =
+      command match {
+        case registerUSerCommand: RegisterUserCommand =>
+          Effect.reply(registerUSerCommand.replyTo)(UnsupportedRegisterCommandUserCommand("Unable to register user, user already registered"))
+      }
 
-    override def applyEvent(event: Event): State = ???
+    override def applyEvent(event: Event): State =
+      event match {
+        case registeredUserEvent: RegisteredUserEvent =>
+          throw new IllegalStateException(s"Unexpected event ${registeredUserEvent.getClass.getName}")
+      }
   }
 
   case class ActiveState(override val id: String) extends State {
