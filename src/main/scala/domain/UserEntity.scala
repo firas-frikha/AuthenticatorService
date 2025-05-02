a6deeceae4bb44b4ea3226df45e0d847fea6991e
firas-frikha <frikha.firas@outlook.com>
2025-05-02 17:08:00 +0200

    finish implementing user entity commands and events

--- src/main/scala/domain/UserEntity.scala
+++ src/main/scala/domain/UserEntity.scala
@@ -36,6 +36,12 @@ object UserEntity {
   case class LoginUserCommand(passwordHash: String)
                              (val replyTo: ActorRef[LoginResult]) extends Command
 
+  case class UnlockUserCommand(newPasswordHash: String)
+                              (val replyTo: ActorRef[UnlockResult]) extends Command
+
+  case class DeleteUserCommand()
+                              (val replyTo: ActorRef[DeleteUserResult]) extends Command
+
   sealed trait Event extends CborSerializable {
     def id: String
   }
@@ -54,8 +60,17 @@ object UserEntity {
   case class UserVerifiedEvent(id: String,
                                verifiedAt: LocalDateTime) extends Event
 
+  case class UserLockedEvent(id: String, lockedAt: LocalDateTime) extends Event
+
   case class UserLoggedInEvent(id: String,
                                loggedInAt: LocalDateTime) extends Event
+
+  case class UserUnlockedEvent(id: String,
+                               newPassword: String) extends Event
+
+
+  case class UserDeletedEvent(id: String,
+                              deletedAt: LocalDateTime) extends Event
 
 
   case class UserLoginFailureEvent(id: String, loginAt: LocalDateTime) extends Event
@@ -84,6 +99,20 @@ object UserEntity {
 
   case object FailedLoginResult extends LoginResult
 
+  case object UserLocked extends LoginResult
+
+  trait UnlockResult extends Result
+
+  case object SuccessfulUnlockCommand extends UnlockResult
+
+  case class UnsupportedUnlockCommand(reason: String) extends UnlockResult
+
+  trait DeleteUserResult extends Result
+
+  case object SuccessfulDeleteCommand extends DeleteUserResult
+
+  case class UnsupportedDeleteCommand(reason: String) extends DeleteUserResult
+
   case class EmptyState(override val id: String) extends State {
 
     override def applyCommand(command: Command): ReplyEffect[Event, State] =
@@ -109,6 +138,12 @@ object UserEntity {
         case loginUserCommand: LoginUserCommand =>
           Effect
             .reply(loginUserCommand.replyTo)(UnsupportedLoginCommand(s"Cannot execute ${loginUserCommand.getClass.getName}, user is not created yet !"))
+        case unlockUserCommand: UnlockUserCommand =>
+          Effect
+            .reply(unlockUserCommand.replyTo)(UnsupportedUnlockCommand(s"Cannot execute ${unlockUserCommand.getClass.getName}, user is not created yet !"))
+        case deleteUserCommand: DeleteUserCommand =>
+          Effect
+            .reply(deleteUserCommand.replyTo)(UnsupportedDeleteCommand(s"Cannot execute ${deleteUserCommand.getClass.getName}, user is not created yet !"))
       }
 
     override def applyEvent(event: Event): State =
@@ -128,6 +163,14 @@ object UserEntity {
           throw new IllegalStateException(s"Unexpected event ${userVerifiedEvent.getClass.getName} in ${this.getClass.getName} state")
         case userLoggedInEvent: UserLoggedInEvent =>
           throw new IllegalStateException(s"Unexpected event ${userLoggedInEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userLockedEvent: UserLockedEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userLockedEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userUnlockedEvent: UserUnlockedEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userUnlockedEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userLoginFailureEvent: UserLoginFailureEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userLoginFailureEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userDeletedEvent: UserDeletedEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userDeletedEvent.getClass.getName} in ${this.getClass.getName} state")
       }
   }
 
@@ -143,8 +186,8 @@ object UserEntity {
 
     override def applyCommand(command: Command): ReplyEffect[Event, State] =
       command match {
-        case registerUSerCommand: RegisterUserCommand =>
-          Effect.reply(registerUSerCommand.replyTo)(UnsupportedRegisterUserCommand("Unable to register user, user in pending verification state"))
+        case registerUserCommand: RegisterUserCommand =>
+          Effect.reply(registerUserCommand.replyTo)(UnsupportedRegisterUserCommand("Unable to register user, user in pending verification state"))
         case verifyUserCommand: VerifyUserCommand =>
           if (verifyUserCommand.verificationToken == verificationToken && LocalDateTime.now().isBefore(tokenExpirationDate))
             Effect
@@ -156,6 +199,12 @@ object UserEntity {
         case loginUserCommand: LoginUserCommand =>
           Effect
             .reply(loginUserCommand.replyTo)(UnsupportedLoginCommand(s"Cannot execute ${loginUserCommand.getClass.getName}, user is pending verification state !"))
+        case unlockUserCommand: UnlockUserCommand =>
+          Effect
+            .reply(unlockUserCommand.replyTo)(UnsupportedUnlockCommand(s"Cannot execute ${unlockUserCommand.getClass.getName}, user is pending verification state !"))
+        case deleteUserCommand: DeleteUserCommand =>
+          Effect
+            .reply(deleteUserCommand.replyTo)(UnsupportedDeleteCommand(s"Cannot execute ${deleteUserCommand.getClass.getName}, user is not created yet !"))
       }
 
     override def applyEvent(event: Event): State =
@@ -175,6 +224,14 @@ object UserEntity {
           throw new IllegalStateException(s"Unexpected event ${registeredUserEvent.getClass.getName} in ${this.getClass.getName} state")
         case userLoggedInEvent: UserLoggedInEvent =>
           throw new IllegalStateException(s"Unexpected event ${userLoggedInEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userUnlockedEvent: UserUnlockedEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userUnlockedEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userLoginFailureEvent: UserLoginFailureEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userLoginFailureEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userLockedEvent: UserLockedEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userLockedEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userDeletedEvent: UserDeletedEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userDeletedEvent.getClass.getName} in ${this.getClass.getName} state")
       }
   }
 
@@ -202,10 +259,22 @@ object UserEntity {
               .persist(UserLoggedInEvent(id = id, loggedInAt = LocalDateTime.now()))
               .thenReply(loginUserCommand.replyTo)(_ => SuccessfulLogin)
           else {
-            Effect
-              .persist(UserLoginFailureEvent(id = id, loginAt = LocalDateTime.now()))
-              .thenReply(loginUserCommand.replyTo)(_ => FailedLoginResult)
+            if (loginAttempts + 1 <= MaximumNumberOfLoginAttempts)
+              Effect
+                .persist(UserLoginFailureEvent(id = id, loginAt = LocalDateTime.now()))
+                .thenReply(loginUserCommand.replyTo)(_ => FailedLoginResult)
+            else
+              Effect
+                .persist(UserLockedEvent(id = id, lockedAt = LocalDateTime.now()))
+                .thenReply(loginUserCommand.replyTo)(_ => UserLocked)
           }
+        case unlockUserCommand: UnlockUserCommand =>
+          Effect
+            .reply(unlockUserCommand.replyTo)(UnsupportedUnlockCommand(s"Cannot execute ${unlockUserCommand.getClass.getName}, user is not locked !"))
+        case deleteUserCommand: DeleteUserCommand =>
+          Effect
+            .persist(UserDeletedEvent(id = id, deletedAt = LocalDateTime.now()))
+            .thenReply(deleteUserCommand.replyTo)(_ => SuccessfulDeleteCommand)
       }
 
     override def applyEvent(event: Event): State =
@@ -214,23 +283,36 @@ object UserEntity {
           throw new IllegalStateException(s"Unexpected event ${registeredUserEvent.getClass.getName} in ${this.getClass.getName} state")
         case userVerifiedEvent: UserVerifiedEvent =>
           throw new IllegalStateException(s"Unexpected event ${userVerifiedEvent.getClass.getName} in ${this.getClass.getName} state")
-        case UserLoginFailureEvent: UserLoginFailureEvent =>
-          val totalNumberOfLoginAttempts = loginAttempts + 1
-          if (totalNumberOfLoginAttempts > MaximumNumberOfLoginAttempts) {
-            LockedUserState(id = id,
-              firstName = firstName,
-              lastName = lastName,
-              userId = userId,
-              email = email,
-              createdAt = createdAt,
-              passwordHash = passwordHash,
-              verifiedAt = verifiedAt,
-              lockedAt = UserLoginFailureEvent.loginAt)
-          } else {
-            copy(loginAttempts = loginAttempts + 1)
-          }
-        case UserLoggedInEvent: UserLoggedInEvent =>
+        case userLoginFailureEvent: UserLoginFailureEvent =>
+          copy(loginAttempts = loginAttempts + 1)
+        case userLoggedInEvent: UserLoggedInEvent =>
           copy(loginAttempts = 0)
+        case userUnlockedEvent: UserUnlockedEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userUnlockedEvent.getClass.getName} in ${this.getClass.getName} state")
+
+        case userLockedEvent: UserLockedEvent =>
+          LockedUserState(id = id,
+            firstName = firstName,
+            lastName = lastName,
+            userId = userId,
+            email = email,
+            createdAt = createdAt,
+            passwordHash = passwordHash,
+            verifiedAt = verifiedAt,
+            lockedAt = userLockedEvent.lockedAt)
+
+        case userDeletedEvent: UserDeletedEvent =>
+          DeletedState(
+            id = id,
+            firstName = firstName,
+            lastName = lastName,
+            userId = userId,
+            email = email,
+            createdAt = createdAt,
+            passwordHash = passwordHash,
+            verifiedAt = verifiedAt,
+            deletedAt = userDeletedEvent.deletedAt
+          )
       }
   }
 
@@ -244,16 +326,85 @@ object UserEntity {
                              verifiedAt: LocalDateTime,
                              lockedAt: LocalDateTime) extends State {
 
-    override def applyCommand(command: Command): ReplyEffect[Event, State] = ???
+    override def applyCommand(command: Command): ReplyEffect[Event, State] =
+      command match {
+        case registerUserCommand: RegisterUserCommand =>
+          Effect.reply(registerUserCommand.replyTo)(UnsupportedRegisterUserCommand("Cannot register user, user already registered"))
+        case verifyUserCommand: VerifyUserCommand =>
+          Effect.reply(verifyUserCommand.replyTo)(UnsupportedVerifyUserCommand("Cannot verify user, user already verified"))
+        case loginUserCommand: LoginUserCommand =>
+          Effect.reply(loginUserCommand.replyTo)(UnsupportedLoginCommand("Cannot login user, user account is locked"))
+        case unlockUserCommand: UnlockUserCommand =>
+          Effect
+            .persist(UserUnlockedEvent(id, unlockUserCommand.newPasswordHash))
+            .thenReply(unlockUserCommand.replyTo)(_ => SuccessfulUnlockCommand)
+        case deleteUserCommand: DeleteUserCommand =>
+          Effect
+            .reply(deleteUserCommand.replyTo)(UnsupportedDeleteCommand(s"Cannot execute ${deleteUserCommand.getClass.getName}, user account is locked !"))
+      }
 
-    override def applyEvent(event: Event): State = ???
+    override def applyEvent(event: Event): State =
+      event match {
+        case registeredUserEvent: RegisteredUserEvent =>
+          throw new IllegalStateException(s"Unexpected event ${registeredUserEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userVerifiedEvent: UserVerifiedEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userVerifiedEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userLoggedInEvent: UserLoggedInEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userLoggedInEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userLoginFailureEvent: UserLoginFailureEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userLoginFailureEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userUnlockedEvent: UserUnlockedEvent =>
+          RegisteredUserState(
+            id = id,
+            firstName = firstName,
+            lastName = lastName,
+            userId = userId,
+            email = email,
+            createdAt = createdAt,
+            passwordHash = userUnlockedEvent.newPassword,
+            verifiedAt = verifiedAt,
+            loginAttempts = 0
+          )
+        case userLockedEvent: UserLockedEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userLockedEvent.getClass.getName} in ${this.getClass.getName} state")
+        case userDeletedEvent: UserDeletedEvent =>
+          throw new IllegalStateException(s"Unexpected event ${userDeletedEvent.getClass.getName} in ${this.getClass.getName} state")
+      }
   }
 
-  case class DeletedState(override val id: String) extends State {
+  case class DeletedState(override val id: String,
+                          firstName: String,
+                          lastName: String,
+                          userId: String,
+                          email: String,
+                          createdAt: LocalDateTime,
+                          passwordHash: String,
+                          verifiedAt: LocalDateTime,
+                          deletedAt: LocalDateTime) extends State {
 
-    override def applyCommand(command: Command): ReplyEffect[Event, State] = ???
+    override def applyCommand(command: Command): ReplyEffect[Event, State] =
+      command match {
+        case registerUserCommand: RegisterUserCommand =>
+          Effect
+            .reply(registerUserCommand.replyTo)(UnsupportedRegisterUserCommand(s"Cannot execute ${registerUserCommand.getClass.getName}, user is in deleted state"))
+        case verifyUserCommand: VerifyUserCommand =>
+          Effect
+            .reply(verifyUserCommand.replyTo)(UnsupportedVerifyUserCommand(s"Cannot execute ${verifyUserCommand.getClass.getName}, user is in deleted state"))
+        case loginUserCommand: LoginUserCommand =>
+          Effect
+            .reply(loginUserCommand.replyTo)(UnsupportedLoginCommand(s"Cannot execute ${loginUserCommand.getClass.getName}, user is in deleted state"))
+        case unlockUserCommand: UnlockUserCommand =>
+          Effect
+            .reply(unlockUserCommand.replyTo)(UnsupportedUnlockCommand(s"Cannot execute ${unlockUserCommand.getClass.getName}, user is in deleted state"))
+        case deleteUserCommand: DeleteUserCommand =>
+          Effect
+            .reply(deleteUserCommand.replyTo)(UnsupportedDeleteCommand(s"Cannot execute ${deleteUserCommand.getClass.getName}, user is in deleted state"))
+      }
 
-    override def applyEvent(event: Event): State = ???
+    override def applyEvent(event: Event): State =
+      event match {
+        case event => throw new IllegalStateException(s"Unexpected event ${event.getClass.getName} in ${this.getClass.getName} state")
+      }
   }
 
   def apply(persistenceId: PersistenceId,
